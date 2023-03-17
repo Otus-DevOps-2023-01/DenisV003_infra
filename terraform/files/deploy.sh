2cc06e4ee0b0d5bebb254806ff29b135fd75a7e8
dvoloshchik <denis.voloshchik@softgamings.com>
2023-03-17 17:30:21 +0300

    Homework Terraform1 completed

--- terraform/files/deploy.sh
+++ terraform/files/deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 APP_DIR=${1:-$HOME}
+sudo apt update
+sleep 5
 sudo apt-get install -y git
 git clone -b monolith https://github.com/express42/reddit.git $APP_DIR/reddit
 cd $APP_DIR/reddit
--- terraform/main.tf
+++ terraform/main.tf
@@ -6,46 +6,46 @@ terraform {
   }
 }
 provider "yandex" {
-  token     = ""
-  cloud_id  = "b1g07pq5lr9osan6ff9e"
-  folder_id = "b1ggnsla1tt7rgor1vsq"
-  zone      = "ru-central1-a"
+  service_account_key_file = var.service_account_key_file
+  #token     = var.ycloud_token
+  cloud_id  = var.cloud_id
+  folder_id = var.folder_id
+  zone      = var.zone
 }
 resource "yandex_compute_instance" "app" {
-  name = "reddit-app"
-  resources {
+  name  = "reddit-app"
+    resources {
     cores  = 2
     memory = 2
-    }
+  }
   boot_disk {
-     initialize_params {
+    initialize_params {
       # Указать id образа созданного в предыдущем домашем задании
-      image_id = "fd8vfbci6kikkq615kn3"
-        }
-      }
+      image_id = var.image_id
+    }
+  }
   network_interface {
     # Указан id подсети default-ru-central1-a
-    subnet_id = "e9baugga0kt6rc1sbhr5"
+    subnet_id = var.subnet_id
     nat       = true
-      }
+  }
   metadata = {
-    ssh-keys = "ubuntu:${file("~/.ssh/id_ed25519.pub")}"
-      }
+    ssh-keys = "ubuntu:${file(var.public_key_path)}"
+  }
   connection {
-    type = "ssh"
-    host = yandex_compute_instance.app.network_interface.0.nat_ip_address
-    user = "ubuntu"
-    agent = true
+    type  = "ssh"
+    host  = yandex_compute_instance.app.network_interface.0.nat_ip_address
+    user  = "ubuntu"
+    agent = false
     # путь до приватного ключа
-    private_key = file("~/.ssh/id_ed25519")
-        }
+    private_key = file(var.private_key)
+  }
   provisioner "file" {
-    source = "files/puma.service"
+    source      = "files/puma.service"
     destination = "/tmp/puma.service"
-    }
-
-    provisioner "remote-exec" {
+  }
+  provisioner "remote-exec" {
     script = "files/deploy.sh"
-    }
+  }
 
 }
